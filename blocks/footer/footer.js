3bf78af65a99722830b17103cf4866fbf3cc1cf7
Erhan Abibula <abibula@adobe.com>
2023-07-20 00:50:00 +0300

    add aria label to footer link

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,16 +1,12 @@
-import { readBlockConfig, decorateIcons } from "../../scripts/lib-franklin.js";
+import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
 /**
  * @param {Element} block
  */
 function decorateCopy(block) {
-  const copyBlock = block.querySelector(".copy");
-  const [startYear, copyText] = [
-    ...copyBlock.querySelectorAll(":scope > div > div"),
-  ];
-  startYear.textContent = `© ${
-    startYear.textContent
-  }-${new Date().getFullYear()}, ${copyText.textContent}`;
+  const copyBlock = block.querySelector('.copy');
+  const [startYear, copyText] = [...copyBlock.querySelectorAll(':scope > div > div')];
+  startYear.textContent = `© ${startYear.textContent}-${new Date().getFullYear()}, ${copyText.textContent}`;
   copyText.remove();
 }
 
@@ -20,24 +16,31 @@ function decorateCopy(block) {
  */
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
-  block.textContent = "";
+  block.textContent = '';
 
   // fetch footer content
-  const footerPath = cfg.footer || "/footer";
+  const footerPath = cfg.footer || '/footer';
   const resp = await fetch(
     `${footerPath}.plain.html`,
-    window.location.pathname.endsWith("/footer") ? { cache: "reload" } : {}
+    window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {},
   );
 
   if (resp.ok) {
     const html = await resp.text();
 
     // decorate footer DOM
-    const footer = document.createElement("div");
+    const footer = document.createElement('div');
     footer.innerHTML = html;
 
     decorateIcons(footer);
     decorateCopy(footer);
     block.append(footer);
+  }
+
+  const pic = block.querySelector('picture');
+  const container = pic?.closest('a');
+
+  if (container) {
+    container.ariaLabel = 'Link to homepage';
   }
 }
