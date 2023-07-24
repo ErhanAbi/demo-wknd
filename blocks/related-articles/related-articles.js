050e6fff6ba5cb64b442d1ffc656be6e1dd25f8e
Erhan Abibula <abibula@adobe.com>
2023-07-24 12:59:27 +0300

    fix double slash when loading related-articles

--- blocks/related-articles/related-articles.js
+++ blocks/related-articles/related-articles.js
@@ -1,32 +1,43 @@
-import { stringToHTML } from "../../scripts/template.js";
+import { stringToHTML } from '../../scripts/template.js';
 
 /**
  * @param {HTMLAnchorElement[]} links
  */
 async function getArticlesMetadata(rawLinks) {
-  const links = rawLinks.map((link) => ({
-    title: link.title || link.textContent,
-    href: `${document.location.origin}/${new URL(link.href).pathname}`,
-  }));
+  const links = rawLinks
+    .filter(link => Boolean(link?.href))
+    .map(link => {
+      let href = `${document.location.origin}/${new URL(link.href).pathname}`;
+      if (href.includes('//')) {
+        href = href.replaceAll('//', '/');
+      }
+      if (!href.endsWith('.plain.html')) {
+        href += '.plain.html';
+      }
+      return {
+        title: link.title || link.textContent,
+        href,
+      };
+    });
 
   const dates = (
     await Promise.all(
-      links.map((link) =>
-        fetch(link.href, { method: "HEAD" }).catch((err) => {
+      links.map(link =>
+        fetch(link.href, { method: 'HEAD' }).catch(err => {
           // console.error(err);
           return null;
-        })
-      )
+        }),
+      ),
     )
-  ).map((req) => {
+  ).map(req => {
     if (req === null) {
       return null;
     }
 
-    if (req.headers.get("last-modified")) {
-      return new Date(req.headers.get("last-modified"));
-    } else if (req.headers.get("date")) {
-      return new Date(req.headers.get("date"));
+    if (req.headers.get('last-modified')) {
+      return new Date(req.headers.get('last-modified'));
+    } else if (req.headers.get('date')) {
+      return new Date(req.headers.get('date'));
     } else {
       return null;
     }
@@ -43,7 +54,7 @@ async function fetchRelatedArticles(relatedArticlesSource) {
   const resp = await fetch(relatedArticlesSource);
   if (!resp.ok) {
     // console.log("response failed", resp);
-    throw new Error("Failed to fetch related articles");
+    throw new Error('Failed to fetch related articles');
   }
 
   const responseData = await resp.json();
@@ -56,30 +67,26 @@ async function fetchRelatedArticles(relatedArticlesSource) {
  * @param {Element} block
  */
 export default async function decorate(block) {
-  const container = block.querySelector(":scope > div > div");
-  const linkToRelatedArticles = container.querySelector("a");
+  const container = block.querySelector(':scope > div > div');
+  const linkToRelatedArticles = container.querySelector('a');
   const articles = await fetchRelatedArticles(linkToRelatedArticles.href);
-  const articleLinks = articles.map((article) =>
-    stringToHTML(
-      `<a href="${article.url}" title="${article.title}">${article.title}</a>`
-    )
+  const articleLinks = articles.map(article =>
+    stringToHTML(`<a href="${article.url}" title="${article.title}">${article.title}</a>`),
   );
 
   // const articleLinks = [...block.querySelectorAll("a")];
   const articlesMetadata = await getArticlesMetadata(articleLinks);
-  const intl = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "medium",
+  const intl = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'medium',
   });
 
   const markup = stringToHTML(
     `<div class="related-articles-list">
         ${articlesMetadata
           .map(
-            (article) =>
+            article =>
               `<a class="related-articles-article" href="${article.href}">
-                <div class="related-articles-articleTitle">${
-                  article.title
-                }</div>
+                <div class="related-articles-articleTitle">${article.title}</div>
                 ${
                   article.lastModified === null
                     ? ``
@@ -87,10 +94,10 @@ export default async function decorate(block) {
                       ${intl.format(article.lastModified)}
                     </div>`
                 }
-            </a>`
+            </a>`,
           )
-          .join("")}
-    </div>`
+          .join('')}
+    </div>`,
   );
 
   container.replaceWith(markup);
