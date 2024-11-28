611727e7128c074b5d11b1221029ec4b2e17e633
Dragos Pitica <dpitica@adobe.com>
2024-11-28 17:17:28 +0200

    Revert to old loader

--- blocks/ai-reply/ai-reply.js
+++ blocks/ai-reply/ai-reply.js
@@ -1,7 +1,11 @@
+import loadComponents from '../../scripts/spire-ui.js';
+
 /**
  * @param {Element} block
  */
 export default async function decorate(block) {
+  await loadComponents();
+
   const handleSubmit = (query) => {
     const searchURL = new URL('/search', document.location.origin);
     searchURL.searchParams.set('q', query);
@@ -13,14 +17,12 @@ export default async function decorate(block) {
     }
   };
 
-  window.addEventListener('ContentAILoaded', () => {
-    const aiResp = document.createElement('demo-ai-response');
-    aiResp.setProps(() => ({
-      queryIndex: 'inside-2',
-      submittedQuery: new URL(document.location).searchParams.get('q'),
-      onSubmit: handleSubmit
-    }));
+  const aiResp = document.createElement('demo-ai-response');
+  aiResp.setProps(() => ({
+    queryIndex: 'inside-2',
+    submittedQuery: new URL(document.location).searchParams.get('q'),
+    onSubmit: handleSubmit
+  }));
 
-    block.appendChild(aiResp);
-  });
+  block.appendChild(aiResp);
 }
