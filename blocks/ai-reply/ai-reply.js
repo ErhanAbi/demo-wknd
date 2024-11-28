da8e6bd45cbb41db908031633c4a8b01e93cdf44
Dragos Pitica <dpitica@adobe.com>
2024-11-28 17:07:04 +0200

    WIP. Conversational look and feel

--- blocks/ai-reply/ai-reply.js
+++ blocks/ai-reply/ai-reply.js
@@ -1,14 +1,26 @@
-import loadComponents from '../../scripts/spire-ui.js';
-
 /**
  * @param {Element} block
  */
 export default async function decorate(block) {
-  await loadComponents();
+  const handleSubmit = (query) => {
+    const searchURL = new URL('/search', document.location.origin);
+    searchURL.searchParams.set('q', query);
 
-  const aiResp = document.createElement('spire-ai-response');
+    if (document.location.pathname === '/search') {
+      window.history.replaceState(null, null, searchURL);
+    } else {
+      window.location = searchURL;
+    }
+  };
 
-  aiResp.setProps(() => ({ queryIndex: 'wkndcf', colorScheme: 'light' }));
+  window.addEventListener('ContentAILoaded', () => {
+    const aiResp = document.createElement('demo-ai-response');
+    aiResp.setProps(() => ({
+      queryIndex: 'inside-2',
+      submittedQuery: new URL(document.location).searchParams.get('q'),
+      onSubmit: handleSubmit
+    }));
 
-  block.appendChild(aiResp);
+    block.appendChild(aiResp);
+  });
 }
